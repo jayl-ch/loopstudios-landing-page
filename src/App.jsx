711734f9edd4b2f62b920c08e018d32aac84be72
jayl-ch <jaychicogo328@gmail.com>
2026-01-03 20:44:23 +0800

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./css/App.css";
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
